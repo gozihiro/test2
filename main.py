21405e35a45a542601fb4db19bc2dfdc51845351
hiroki <zx1706@hotmail.co.jp>
2019-08-12 22:49:18 +0900

    test

--- main.py
+++ main.py
@@ -23,11 +23,22 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
+#from linebot.models import (
+#    MessageEvent, TextMessage, TextSendMessage,
+#)
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URITemplateAction,
+    PostbackTemplateAction, DatetimePickerTemplateAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
 )
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
@@ -39,18 +50,23 @@ if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
+print('secret:' + channel_secret)
+print('token:' + channel_access_token)
+
+
+
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 
-@app.route("/callback", methods=['POST'])
+@application.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    application.logger.info("Request body: " + body)
 
     # handle webhook body
     try:
@@ -63,12 +79,24 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
 
+    #ユーザの発言
+    usermessage=event.message.text
+
+
+    #メッセージ
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text='ありがとうございました。また来てください。(メガロス)'))
+
+
+#加入しない場合、全部出す
+@handler.add(PostbackEvent)
+def handle_postback(event):
+
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text='iiiii')
+    )
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    application.run(host="0.0.0.0", port=port)
